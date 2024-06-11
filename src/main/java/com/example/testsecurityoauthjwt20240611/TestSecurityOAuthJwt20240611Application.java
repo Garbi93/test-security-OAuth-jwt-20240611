f05ba516ea1fde5f6c4c2e0c60e8bb07b86fa704
Garbi93 <iseung2093@gmail.com>
2024-06-11 16:51:18 +0900

    06/11 스프링 OAuth2 클라이언트 JWT 3: 프로젝트 생성 및 의존성 추가

---
+++ src/main/java/com/example/testsecurityoauthjwt20240611/TestSecurityOAuthJwt20240611Application.java
@@ -0,0 +1,13 @@
+package com.example.testsecurityoauthjwt20240611;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSecurityOAuthJwt20240611Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSecurityOAuthJwt20240611Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/testsecurityoauthjwt20240611/TestSecurityOAuthJwt20240611ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testsecurityoauthjwt20240611;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSecurityOAuthJwt20240611ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
